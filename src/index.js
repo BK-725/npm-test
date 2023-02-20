6f9191576d33fe15120847e45406692cd33a21d1
bk <80371033@qq.com>
2023-02-20 18:28:41 +0800

    测试npm包方法

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
-import { vueText } from './vue-test/index'
-export { vueText }
+// 测试 vue 组件
+export { default as ddVueTest } from './vue-test/index.js'
+
+// 测试 js 吐司方法
+export { default as ddToast } from './js-test/'
